58108fdd9eaaeed01b707cc998a54b65127d22e7
Allan Simon <allan.simon@supinfo.com>
2014-06-07 21:46:55 +0800

    Update Version.h

--- src/Version.h
+++ src/Version.h
@@ -12,6 +12,6 @@
 #include <string>
 
 // See CHANGELOG for a list of what has changed between versions
-static const std::string LinthesiaVersionString = "0.4.2";
+static const std::string LinthesiaVersionString = "0.4.3";
 
 #endif // __LINTHESIA_VERSION_H__
