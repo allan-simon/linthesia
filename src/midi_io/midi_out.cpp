d391c5bab10a9017585471d0741f36cb876d1bbc
Allan SIMON <allan.simon@supinfo.com>
2014-06-22 00:40:54 +0800

    removed some debug output

--- src/midi_io/midi_out.cpp
+++ src/midi_io/midi_out.cpp
@@ -19,10 +19,6 @@ void MidiOut::write(const MidiEvent &out) {
     message[0] = out.get_status_code();
     message[1] = static_cast<unsigned char>(out.get_note_number());
     message[2] = 90;//static_cast<unsigned char>(out.get_note_velocity());
-    std::cout << static_cast<int>(message[0]) << std::endl;
-    std::cout << out.get_note_number() << std::endl;
-    std::cout << out.get_note_velocity() << std::endl;
-    std::cout <<std::endl;
     rtMidiOut.sendMessage(&message);
 }
 
