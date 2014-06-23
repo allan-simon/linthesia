d1d3e559d1dbf937c8214fdf16b717840d3cc168
Allan Simon <simona@gobeta.com.cn>
2014-06-23 23:22:18 +0800

    added a back button on select track screen

--- src/screens/select_track_screen/select_track_screen.cpp
+++ src/screens/select_track_screen/select_track_screen.cpp
@@ -17,13 +17,23 @@ const static auto BACKGROUND_COLOR = sf::Color(64, 64, 64);
 /**
  *
  */
+SelectTrackScreen::SelectTrackScreen() :
+    AbstractScreen(),
+    backButton("back")
+{
+
+}
+
+/**
+ *
+ */
 ScreenIndex SelectTrackScreen::run(
     sf::RenderWindow &app,
     Context &context
 ) {
     sf::Event event;
 
-    std::cout << INDEX << std::endl;
+    setBackButtonPosition(app);
 
     // on purpose
     while (true) {
@@ -31,11 +41,34 @@ ScreenIndex SelectTrackScreen::run(
             if (event.type == sf::Event::Closed) {
                 return STOP_APPLICATION;
             }
+
+            if (backButton.actionTriggered(app, event)) {
+                return START_APPLICATION;
+            }
+
+
         }
+
         app.clear(BACKGROUND_COLOR);
+        app.draw(backButton);
         app.display();
+
     }
+
 }
+
+/**
+ *
+ */
+void SelectTrackScreen::setBackButtonPosition(const sf::RenderWindow &app) {
+    // put the "back" putton at bottom left (with some padding)
+    backButton.setPosition(
+        PADDING,
+        app.getSize().y - (backButton.getGlobalBounds().height + PADDING)
+    );
+}
+
+
 
 } // end namespace linthesia
 
--- src/screens/select_track_screen/select_track_screen.h
+++ src/screens/select_track_screen/select_track_screen.h
@@ -4,16 +4,29 @@
 #include "screens/abstract_screen.h"
 #include "screens/screens.h"
 
+#include "buttons/short_one_line_button.h"
+
 namespace linthesia {
 
 class SelectTrackScreen : public AbstractScreen {
 
     public:
         const static ScreenIndex INDEX;
+
+        SelectTrackScreen();
+
         ScreenIndex run(
             sf::RenderWindow &app,
             Context &context
         );
+
+    protected:
+        /**
+         * Set the position of the back button on main screen
+         */
+        void setBackButtonPosition(const sf::RenderWindow &app);
+        linthesia::ShortOneLineButton backButton;
+
 
 };
 
