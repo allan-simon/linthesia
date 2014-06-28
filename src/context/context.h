e23783ca4c82cfbee5a66916f46af57af3c29b07
Allan Simon <simona@gobeta.com.cn>
2014-06-28 17:45:28 +0800

    re #33 tracks options are now stored in Context

--- src/context/context.h
+++ src/context/context.h
@@ -5,6 +5,7 @@
 #include "libmidi/midi.h"
 #include "midi_io/midi_out.h"
 #include "midi_io/midi_in.h"
+#include "track_options/track_options.h"
 
 namespace linthesia {
 
@@ -22,6 +23,7 @@ class Context {
 
         linthesia::MidiOut midiOut;
         linthesia::MidiIn midiIn;
+        linthesia::TrackOptions tracksOptions;
     private:
         Midi* midi;
         std::string filename;
