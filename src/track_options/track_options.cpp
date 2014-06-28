fc518fc0ed80ad6ae6568a1325bd48a9d565e046
Allan Simon <simona@gobeta.com.cn>
2014-06-28 12:23:06 +0800

    close #32 added TrackOptions to store state of options for all tracks

---
+++ src/track_options/track_options.cpp
@@ -0,0 +1,78 @@
+#include <string>
+#include "track_options.h"
+
+namespace linthesia {
+
+/**
+ *
+ */
+TrackOptions::TrackOptions() {
+    style2name[PlayStyle::AUTO_PLAY] = "played automatically";
+    style2name[PlayStyle::NOT_PLAYED] = "not played";
+    style2name[PlayStyle::PLAYED_BY_PLAYER] = "played manually";
+}
+
+/**
+ *
+ */
+const std::string TrackOptions::toPreviousStyle(unsigned trackId) {
+
+    if (trackidStyle.find(trackId) == trackidStyle.end()) {
+        trackidStyle[trackId] = PlayStyle::AUTO_PLAY;
+    }
+
+    switch (trackidStyle[trackId]) {
+        case PlayStyle::AUTO_PLAY:
+            trackidStyle[trackId] = NOT_PLAYED;
+            break;
+
+        case PlayStyle::PLAYED_BY_PLAYER:
+            trackidStyle[trackId] = AUTO_PLAY;
+            break;
+
+        case PlayStyle::NOT_PLAYED:
+            trackidStyle[trackId] = PLAYED_BY_PLAYER;
+            break;
+    }
+
+    return style2name[trackidStyle[trackId]];
+    
+}
+
+/**
+ *
+ */
+const std::string TrackOptions::toNextStyle(unsigned trackId) {
+
+    if (trackidStyle.find(trackId) == trackidStyle.end()) {
+        trackidStyle[trackId] = PlayStyle::AUTO_PLAY;
+    }
+
+    switch (trackidStyle[trackId]) {
+        case PlayStyle::AUTO_PLAY:
+            trackidStyle[trackId] = PLAYED_BY_PLAYER;
+            break;
+
+        case PlayStyle::PLAYED_BY_PLAYER:
+            trackidStyle[trackId] = NOT_PLAYED;
+            break;
+
+        case PlayStyle::NOT_PLAYED:
+            trackidStyle[trackId] = AUTO_PLAY;
+            break;
+    }
+
+    return style2name[trackidStyle[trackId]];
+}
+
+/**
+ *
+ */
+PlayStyle TrackOptions::getStyle(unsigned trackId) {
+    return trackidStyle[trackId];
+}
+
+
+
+}
+
---
+++ src/track_options/track_options.h
@@ -0,0 +1,61 @@
+#ifndef LINTHESIA_TRACK_OPTIONS_H
+#define LINTHESIA_TRACK_OPTIONS_H
+
+#include <string>
+#include <map>
+
+namespace linthesia {
+
+
+enum PlayStyle {
+    AUTO_PLAY, // played by computer
+    PLAYED_BY_PLAYER, // supposed to be played by human player
+    NOT_PLAYED, // mute
+};
+
+
+
+/**
+ * Store and permit to modify the different game options for each
+ * tracks (if track is played/mute/played by player etc.)
+ */
+class TrackOptions {
+
+    public:
+        TrackOptions();
+
+        /**
+         * Change the play style of given track to the "previous one"
+         * (see PlayStyle enum) and return the Human name of this new
+         * style
+         */
+        const std::string toPreviousStyle(unsigned trackId);
+
+        /**
+         * Change the play style of given track to the "next one"
+         * (see PlayStyle enum) and return the Human name of this new
+         * style
+         */
+        const std::string toNextStyle(unsigned trackId);
+
+        /**
+         *
+         */
+        PlayStyle getStyle(unsigned trackId);
+
+    private:
+        /**
+         * store the track style by trackId
+         */
+        std::map<unsigned, PlayStyle> trackidStyle;
+
+        /**
+         * map a style to its human readable name
+         */
+        std::map<PlayStyle, std::string> style2name;
+
+};
+
+} // end of namespace
+
+#endif
